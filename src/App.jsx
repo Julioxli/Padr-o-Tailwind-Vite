60c294fcc8f3ac630d700a5c2e7f749721fa97d6
Júlio Xavier <115498048+Julioxli@users.noreply.github.com>
2023-04-02 16:52:39 -0300

    Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import { Outlet } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+    <Navbar />
+    <Outlet />
+    </div>
   )
 }
 
